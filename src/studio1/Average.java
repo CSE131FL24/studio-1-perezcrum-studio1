a8c623bb13536f2fd74d890cf553027996fdf431
diego <diego@DiegoPerez420.dhcp.wustl.edu>
2024-09-04 14:16:54 -0500

    Final Studio1 Commits as of 9/4/2024 by Diego P and Anne C

---
+++ src/studio1/Average.java
@@ -0,0 +1,25 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("The first of two integers to be averaged?");
+		int n1 = in.nextInt();
+		System.out.println("The second of two integers to be averaged?");
+		int n2 = in.nextInt();
+	
+		
+		double sum = (n1 + n2);
+		double count = 2.0;
+		double average = sum/count;
+		System.out.println("The average of " + n1 + " and " + n2 + " is " + (average) + ".");
+		
+		
+	}
+
+}
